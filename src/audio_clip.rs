12de60e3891239dc89f8bcbc38b116c0af0da557
董家宏 <dongjiahong@hotmail.com>
2022-02-08 14:10:41 +0800

    update db_rs

--- src/audio_clip.rs
+++ src/audio_clip.rs
@@ -1,6 +1,6 @@
 use chrono::prelude::*;
 use color_eyre::eyre::{eyre, Result};
-use cpal::traits::{DeviceTrait, HostTrait};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use dasp::{interpolate::linear::Linear, signal, Signal};
 use std::sync::{mpsc::Sender, Arc, Mutex};
 
@@ -79,7 +79,102 @@ impl AudioClip {
         }
 
         let stream = match config.sample_format() {
-            cpal::SampleFormat::F32 => device.build_input_stream(&config.into(),move |data, _: &_| write_input_data::<f32>(data,channels,&clip_2),err_fn)?,
+            cpal::SampleFormat::F32 => device.build_input_stream(
+                &config.into(),
+                move |data, _: &_| write_input_data::<f32>(data, channels, &clip_2),
+                err_fn,
+            )?,
+            cpal::SampleFormat::I16 => device.build_input_stream(
+                &config.into(),
+                move |data, _: &_| write_input_data::<i16>(data, channels, &clip_2),
+                err_fn,
+            )?,
+            cpal::SampleFormat::U16 => device.build_input_stream(
+                &config.into(),
+                move |data, _: &_| write_input_data::<u16>(data, channels, &clip_2),
+                err_fn,
+            )?,
+        };
+
+        stream.play()?;
+
+        let (tx, rx) = channel();
+        ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))?;
+
+        println!("Waiting for Ctrl-C...");
+        rx.recv()?;
+        println!("Got it! Exiting...");
+
+        drop(stream);
+        let clip = clip.lock().unwrap().take().unwrap();
+
+        eprintln!("Recorded {} samples", clip.samples.len());
+        Ok(clip)
+    }
+
+    pub fn play(&self) -> Result<()> {
+        let host = cpal::default_host();
+        let device = host
+            .default_output_device()
+            .ok_or_else(|| eyre!("No output device"))?;
+        println!("Output device: {}", device.name()?);
+        let config = device.default_input_config()?;
+
+        println!("Begin playback...");
+
+        type StateHandle = Arc<Mutex<Option<(usize, Vec<f32>, Sender<()>)>>>;
+        let sample_rate = config.sample_rate().0;
+        let (done_tx, done_rx) = channel::<()>();
+        let state = (0, self.resample(sample_rate).samples, done_tx);
+        let state = Arc::new(Mutex::new(Some(state)));
+        let channels = config.channels();
+
+        let err_fn = move |err| {
+            eprintln!("an error occurred on stream: {}", err);
+        };
+
+        fn write_output_data<T>(output: &mut [T], channels: u16, writer: &StateHandle)
+        where
+            T: cpal::Sample,
+        {
+            if let Ok(mut guard) = writer.try_lock() {
+                if let Some((i, clip_samples, done)) = guard.as_mut() {
+                    for frame in output.chunks_mut(channels.into()) {
+                        for sample in frame.iter_mut() {
+                            *sample = cpal::Sample::from(clip_samples.get(*i).unwrap_or(&0f32));
+                        }
+                        *i += 1;
+                    }
+                    if *i >= clip_samples.len() {
+                        if let Err(_) = done.send(()) {
+                            // Playback has already stopped. We'll be dead soon.
+                        }
+                    }
+                }
+            }
         }
+
+        let stream = match config.sample_forma() {
+            cpal::SampleFormat::F32 => device.build_output_stream(
+                &config.into(),
+                move |data, _: &_| write_output_data::<f32>(data, channels, &state),
+                err_fn,
+            )?,
+            cpal::SampleFormat::I16 => device.build_output_stream(
+                &config.into(),
+                move |data, _: &_| write_output_data::<i16>(data, channels, &state),
+                err_fn,
+            )?,
+            cpal::SampleFormat::U16 => device.build_output_stream(
+                &config.into(),
+                move |data, _: &_| write_output_data::<u16>(data, channels, &state),
+                err_fn,
+            )?,
+        };
+
+        stream.play()?;
+
+        done_rx.recv()?;
+        Ok(())
     }
 }
